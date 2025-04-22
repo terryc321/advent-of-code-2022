1a4a4fb50023df5a0cfe86d570470707c774817b
terry cadd <terry.cadd@gmail.com>
2025-04-22 14:18:27 +0100

    trying to figure out search pattern

--- day19/chicken/fun.scm
+++ day19/chicken/fun.scm
@@ -99,61 +99,82 @@ delay buying ... until never ... never bought anything ... cannot make geodes
 |#
 
 
-
-
-(define (blue1 ro rc rob rg robot-ore ore robot-clay clay robot-obs obs robot-geo geo step)
+(define (blue1a robot-ore ore robot-clay clay robot-obs obs robot-geo geo step path)
   (cond
    ((< ore 0) #f)
    ((< clay 0) #f)
    ((< obs 0) #f)
    ((< geo 0) #f)
-   ((and (> geo 0) (> step 24))
+   ((and (> geo 0) (> step 23))
     (when (> geo max-geo)
       (set! max-geo geo)
-      (format #t "we have ~a geode~%" max-geo)))
-   ((> step 24) #f)
+      (format #t "we have ~a geode~%" max-geo)
+      (format #t "~a~%" (reverse path))))
+   ((> step 23) #f)
    (#t
-      (format #t "~%step ~a : robots (ore ~a ~a) (clay ~a ~a) : " step robot-ore ore robot-clay clay)
-      
-      ;; buy an obsidian
-      (when (and (>= ore 3)(>= clay 14))
-	(blue1 ro rc (+ 1 rob) rg
-	       robot-ore  (- ore 3)
-	       robot-clay (- clay 14)
-	       robot-obs  obs
-	       robot-geo  geo
-	       step))
+    ;; feedback
+    ;;(format #t "step ~a : ore:(~a ~a) clay:(~a ~a) obs:(~a ~a) geo:(~a ~a)~%" step robot-ore ore robot-clay clay robot-obs obs robot-geo geo)    
+    (blue1b 0 0 0 0 robot-ore ore robot-clay clay robot-obs obs robot-geo geo step path))))
 
-      ;; buy a clay robot for 2 ore
-      (when (>= ore 2) 
-	  (blue1 ro (+ 1 rc) rob rg
-	   robot-ore (- ore 2)
-	   robot-clay clay
-	   robot-obs  obs
-	   robot-geo geo
-	   step))
 
-      ;; buy an ore robot for 4 ore
-      (when (>= ore 4)
-	(blue1
-	 (+ ro 1) rc rob rg
-	 robot-ore (- ore 4)
-	 robot-clay clay
-	 robot-obs  obs
-	 robot-geo geo
-	 step))
 
-      ;; do nothing
-      (let ((robot-ore (+ ro robot-ore))
-	    (robot-clay (+ rc robot-clay))
-	    (robot-obs (+ rob robot-obs))
-	    (robot-geo (+ rg robot-geo)))    
-	(blue1 0 0 0 0       
-	       robot-ore (+ robot-ore ore)
-	       robot-clay (+ robot-clay clay)
-	       robot-obs  (+ robot-obs obs)
-	       robot-geo (+ robot-geo geo)
-	       (+ 1 step))))))
+
+(define (blue1b ro rc rob rg robot-ore ore robot-clay clay robot-obs obs robot-geo geo step path)
+  
+  ;; buy a geode
+  (when (and (>= ore 2)(>= obs 7))
+    (blue1b ro rc rob (+ 1 rg)
+	    robot-ore  (- ore 2)
+	    robot-clay clay
+	    robot-obs  (- obs 7)
+	    robot-geo  geo
+	    step
+	    (cons 'buy-geode path)
+	    ))
+  
+  ;; buy an obsidian
+  (when (and (>= ore 3)(>= clay 14))
+    (blue1b ro rc (+ 1 rob) rg
+	    robot-ore  (- ore 3)
+	    robot-clay (- clay 14)
+	    robot-obs  obs
+	    robot-geo  geo
+	    step
+	    (cons 'buy-obsidian path)
+	    ))
+
+  ;; buy a clay robot for 2 ore
+  (when (>= ore 2) 
+    (blue1b ro (+ 1 rc) rob rg
+	    robot-ore (- ore 2)
+	    robot-clay clay
+	    robot-obs  obs
+	    robot-geo geo
+	    step
+	    (cons 'buy-clay path)
+	    ))
+
+  ;; buy an ore robot for 4 ore
+  (when (>= ore 4)
+    (blue1b
+     (+ ro 1) rc rob rg
+     robot-ore (- ore 4)
+     robot-clay clay
+     robot-obs  obs
+     robot-geo geo
+     step
+     (cons 'buy-ore path)
+     ))
+
+  ;; do nothing - advance with current layout
+  (blue1a 
+   (+ ro robot-ore) (+ robot-ore ore)
+   (+ rc robot-clay) (+ robot-clay clay)
+   (+ rob robot-obs)  (+ robot-obs obs)
+   (+ rg robot-geo) (+ robot-geo geo)
+   (+ 1 step)
+   (cons 'next path)
+   ))
 
 
 
@@ -167,13 +188,113 @@ delay buying ... until never ... never bought anything ... cannot make geodes
 	 (robot-clay 0)(clay 0)
 	 (robot-obs 0)(obs 0)
 	 (robot-geo 0)(geo 0)
-	 (step 0))
-    (blue1 ro rc rob rg
+	 (step 0)
+	 (path '()))
+    (blue1a 
 	   robot-ore ore
 	   robot-clay clay
 	   robot-obs obs
 	   robot-geo geo
-	   step)))
+	   step
+	   path
+	   )))
 
-;;(blue1-run)
 
+
+(define (replay xs)
+  (let ((ore-robot 1)(ore 0)
+	(clay-robot 0)(clay 0)
+	(obsidian-robot 0)(obsidian 0)
+	(geode-robot 0)(geode 0)
+	(o-r 0)(c-r 0)(obs-r 0)(g-r 0)
+	(step 0))
+    (replay2 xs
+	     step
+	     o-r c-r obs-r g-r
+	     ore-robot ore
+	     clay-robot clay
+	     obsidian-robot obsidian
+	     geode-robot geode)))
+
+
+;; check replay 
+;; Each ore robot costs 4 ore.
+;; Each clay robot costs 2 ore.
+;; Each obsidian robot costs 3 ore and 14 clay.
+;; Each geode robot costs 2 ore and 7 obsidian.
+(define (replay2 xs
+		 step
+		 o-r c-r obs-r g-r
+		 ore-robot ore
+		 clay-robot clay
+		 obsidian-robot obsidian
+		 geode-robot geode)
+  (format #t "step ~a : ~a ~a ~a ~a : ore(~a ~a) clay(~a ~a) obs(~a ~a) geo(~a ~a) ~%"
+	  step
+	  o-r c-r obs-r g-r
+	  ore-robot ore
+	  clay-robot clay
+	  obsidian-robot obsidian
+	  geode-robot geode
+	  )
+  (cond
+   ((null? xs) (list geode))
+   (#t (let ((op (car xs)))
+	 (cond
+	  ((eq? op 'next)
+	   (replay2 (cdr xs)
+		    (+ step 1)
+		    0 0 0 0
+		    (+ o-r ore-robot) (+ ore ore-robot)
+		    (+ c-r clay-robot) (+ clay clay-robot)
+		    (+ obs-r obsidian-robot) (+ obsidian obsidian-robot)
+		    (+ g-r geode-robot) (+ geode geode-robot)))
+	  ((eq? op 'buy-ore) ;; ore costs 4 ... do we have enough ore
+	   (when (< ore 4) (error "cannot buy ore robot do not have enough!~%"))
+	   (format #t "bought ore robot for 4 ore ~%")
+	   (replay2 (cdr xs)
+		    step
+		    (+ o-r 1) c-r obs-r g-r
+		    ore-robot (- ore 4)
+		    clay-robot clay
+		    obsidian-robot obsidian
+		    geode-robot geode))
+	  ((eq? op 'buy-clay) ;; clay costs 2 ore
+	   (when (< ore 2) (error "cannot buy clay robot do not have enough!~%"))
+	   (format #t "bought clay robot for 2 ore ~%")
+	   (replay2 (cdr xs)
+		    step
+		    o-r (+ 1 c-r) obs-r g-r
+		    ore-robot (- ore 2)
+		    clay-robot clay
+		    obsidian-robot obsidian
+		    geode-robot geode))
+	  ((eq? op 'buy-obsidian) ;; obsidian cost 3 ore and 14 clay
+	   (when (or (< ore 3)(< clay 14)) (error "cannot buy obsidian robot do not have enough!~%"))
+	   (format #t "bought obsidian robot for 3 ore and 14 clay ~%")
+	   (replay2 (cdr xs)
+		    step
+		    o-r c-r (+ 1 obs-r) g-r
+		    ore-robot (- ore 3)
+		    clay-robot (- clay 14)
+		    obsidian-robot obsidian
+		    geode-robot geode))
+	  ((eq? op 'buy-geode) ;; geode cost 2 ore and 7 obsidian
+	   (when (or (< ore 2)(< obsidian 7 )) (error "cannot buy geode robot do not have enough!~%"))
+	   (format #t "bought geode robot for 2 ore and 7 obsidian ~%")
+	   (replay2 (cdr xs)
+		    step
+		    o-r c-r obs-r (+ 1 g-r)
+		    ore-robot (- ore 2)
+		    clay-robot clay
+		    obsidian-robot (- obsidian 7)
+		    geode-robot geode))
+	  (#t
+	   (error (format #f "unrecog operator ~a ~%" op))))))))
+
+
+(define (check)
+  (replay '(next next buy-clay next next buy-clay next next buy-clay next next buy-clay next next next buy-obsidian next next next buy-obsidian next next next buy-geode next next buy-obsidian next next buy-geode next next buy-geode next next)))
+
+
+(blue1-run)
