45172ae7f081bfe781899a47623d67309e3f3e9f
terry cadd <terry.cadd@gmail.com>
2025-04-22 10:05:21 +0100

    added haskell and chicken directories

---
+++ day19/haskell/app/Main.hs
@@ -0,0 +1,28 @@
+
+
+
+{-
+how can make documentation from this file ?
+advent of code 2022 day 19 
+
+
+
+Blueprint 1:
+  Each ore robot costs 4 ore.
+  Each clay robot costs 2 ore.
+  Each obsidian robot costs 3 ore and 14 clay.
+  Each geode robot costs 2 ore and 7 obsidian.
+
+Blueprint 2:
+  Each ore robot costs 2 ore.
+  Each clay robot costs 3 ore.
+  Each obsidian robot costs 3 ore and 8 clay.
+  Each geode robot costs 3 ore and 12 obsidian.
+-}
+
+
+module Main where
+
+
+main :: IO ()
+main = putStrLn "Hello, Haskell!"
