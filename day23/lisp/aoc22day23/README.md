05e82ab26673fb01c81aed1d91b0b544283fa686
terry cadd <terry.cadd@gmail.com>
2025-04-27 18:48:26 +0100

    have 2d grid up and running with show , verified input output preserved

--- day23/lisp/aoc22day23/README.md
+++ day23/lisp/aoc22day23/README.md
@@ -7,3 +7,29 @@ This is a project to do ... something.
 
 Specify license here
 
+## Verify input output
+
+parse input and produce output . run a diff against both . should be same
+
+## Multidimensional arrays
+
+can we not have arrays from 1 to n inclusive and array dimensions report size of
+array n by n 
+
+## Iterate over a string
+
+instead of looping using an index from 0 to len - 1 then taking char of string instead
+coerce the string to a list and use loop for in  
+
+- [X] No potential to char wrong index
+- [X] Less code
+
+```lisp
+	(loop for x from 0 to (- len 1) do
+	  (let ((ch (char line x)))
+```
+
+```lisp
+	(loop for ch in (coerce line 'list) do
+```
+
--- day23/lisp/aoc22day23/aoc22day23.lisp
+++ day23/lisp/aoc22day23/aoc22day23.lisp
@@ -1,6 +1,91 @@
 ;;;; aoc22day23.lisp
 
+(sb-ext:restrict-compiler-policy 'debug 3 3)
+(sb-ext:restrict-compiler-policy 'safety 3 3)
+
 (in-package #:aoc22day23)
+
+;;; parsing phase
+
+#|
+task : read a file and turn grid into something more usable
+[ ] open a file for reading
+[ ] read line by line
+[ ] determine playfield width height , ignoring external walls
+[ ] player at 1 0 assumed
+[ ] player exit at width (height + 1)
+|#
+
+;; each line should be same length
+;; read-line chomps newline i presume
+;; (parse-file "../../input.txt")
+(defun parse-file (filename)
+  (with-open-file (stream filename :direction :input)
+    (let ((lines '()))
+      (catch 'foo
+	(loop while t do
+	  (let ((line (read-line stream nil 'eof)))
+	    (cond
+	      ((eq line 'eof) (throw 'foo t))
+	      (t (setq lines (cons line lines)))))))
+      (setq lines (reverse lines))
+      lines)))
+
+
+;; plain square grid
+(defun parse-lines (lines)
+  (let* ((width (length (car lines)))
+	 (height (length lines))
+	 (x 0)
+	 (y 0)
+	 (grid (make-array (list (+ width 1)(+ height 1)) :initial-element #\.)))    
+    (dolist (line lines)
+      (incf y)
+      (setq x 0)
+      (let ((len (length line)))
+	;; each line has same length
+	(assert (= len width))
+	(loop for ch in (coerce line 'list) do
+	  (incf x)
+	  (assert (or (char= ch #\.)(char= ch #\#)))
+	  (setf (aref grid x y) ch))))
+    grid))
+
+(defun parse (filename)
+  (parse-lines (parse-file filename)))
+
+(defun test-parse ()
+  (let ((lines (parse-file "../../input.txt")))
+    (format t "lines => ~a~%" lines)
+    (parse-lines lines)))
+
+(defun grid-width (grid)
+  (destructuring-bind (width height) (array-dimensions grid)
+    (- width 1)))
+
+(defun grid-height (grid)
+  (destructuring-bind (width height) (array-dimensions grid)
+    (- height 1)))
+
+(defun grid-dimensions (grid)
+  (destructuring-bind (width height) (array-dimensions grid)
+    (list (- width 1)(- height 1))))
+
+
+(defun show (grid)
+  (let ((width (grid-width grid))
+	(height (grid-height grid)))
+    (loop for y from 1 to height do
+      (format t "~%")
+      (loop for x from 1 to width do
+	(let ((ch (aref grid x y)))
+	  (format t "~a" ch))))
+    (format t "~%")))
+
+
+
+
+
 
 
 
