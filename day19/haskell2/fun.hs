67c2d4dbf850d3479990ea2281e066092c05b09e
terry cadd <terry.cadd@gmail.com>
2025-05-29 19:55:41 +0100

    added haskell version 2

---
+++ day19/haskell2/fun.hs
@@ -0,0 +1,34 @@
+
+{-
+aoc 2022 day 19
+
+start with 1 ore robot
+
+Blueprint 1:
+  Each ore robot costs 4 ore.
+  Each clay robot costs 2 ore.
+  Each obsidian robot costs 3 ore and 14 clay.
+  Each geode robot costs 2 ore and 7 obsidian.
+
+Blueprint 2:
+  Each ore robot costs 2 ore.
+  Each clay robot costs 3 ore.
+  Each obsidian robot costs 3 ore and 8 clay.
+  Each geode robot costs 3 ore and 12 obsidian.
+
+choice at each time step
+
+initial state
+start with 0 ore and 1 ore robot
+0 clay and 0 clay robot
+0 obsidian and 0 obsidian robots
+0 geode and 0 geode robots
+
+have to figure out at each time step what is best choice 
+
+after 24 minutes
+
+-}
+
+
+
